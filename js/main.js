01401cb7a88a6318740aa7a58eb1e638704ceece
Brittany Bellanca <brittanybellanca@gmail.com>
2018-07-15 02:07:22 -0400

    played with heading styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+$(document).ready(function(){
+    var largestHeader = $('h1');
+    largestHeader.css({
+        'font-size': '100px',
+        'color': 'green',
+        'margin-bottom': '145px'
+    });
+
+    $('h2').addClass('second-header');
+
+    // $('h1').append(' This is appended.');
+
+    // $('h1').prepend('This is prepended. ');
+
+    $('h1').after('This is after.');
+
+    $('h1').before('This is before.');
+
+});
+
+$(function() {
+    $('p').removeClass('new-class');
+
+    $('h1').on('click', function(e) {
+        // $(this).removeClass('second-header');
+        $(this).slideUp(10000);
+    });
+});
+
+
+
 // $(document).ready(function(){
 //   var listItem = $('li');
 //   // listItem.css({
